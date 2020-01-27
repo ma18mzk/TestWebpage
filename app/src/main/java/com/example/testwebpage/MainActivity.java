9102ce52c8c5f4a823be511ae7e239b5f17c9a84
MZNK <1706960@brunel.ac.uk>
2020-01-27 03:17:18 +0000

    Initial commit

---
+++ app/src/main/java/com/example/testwebpage/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.testwebpage;
+
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = (WebView) findViewById(R.id.webview);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://www.google.com");
+
+
+    }
+}
